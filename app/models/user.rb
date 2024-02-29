633a56b46f113d681e5693090f793785d133ecf0
oishi-yuma-99 <138931627+oishi-yuma-99@users.noreply.github.com>
2024-02-29 10:09:35 +0000

    フォロー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,32 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  
   has_one_attached :profile_image
+  
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-
+  
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+  
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+  
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
+  
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
   
   def get_profile_image
     unless profile_image.attached?
